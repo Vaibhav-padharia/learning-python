91154d3643b8c9d60b8cf9ac505e2b809868e2e8
Vaibhav-padharia <160303912+Vaibhav-padharia@users.noreply.github.com>
2025-01-18 21:08:51 +0530

    Add files via upload

---
+++ modules  1.py
@@ -0,0 +1,10 @@
+import pyjokes
+joke=pyjokes.get_joke()
+print (joke)
+
+import flask
+print(flask.__version__)
+
+
+
+import scipy
---
+++ new.py
@@ -0,0 +1,8 @@
+a=int(input("Type the first number: "))
+b=int(input("Type the second number: "))
+print("The sum of two numbers is:", (a+b))
+print("The division of two numbers is:", (a/b))
+print("The multiplication of two numbers is:", (a*b))
+print("The subtraction of two numbers is:", (a-b))
+
+  
