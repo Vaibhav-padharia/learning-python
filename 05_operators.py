123cc5e4bfc5f2c73ae355b420c8425b76c17e02
Vaibhav-padharia <160303912+Vaibhav-padharia@users.noreply.github.com>
2025-01-23 22:21:05 +0530

    Add 05_operators.py with examples of arithmetic, assignment, comparison, and logical operators

---
+++ 05_operators.py
@@ -0,0 +1,33 @@
+# Arithmetic operators
+a=24
+b=25
+print(a+b) # addition
+print(a*b) # multiplication
+print(a/b) # division
+print(a-b) # subtraction
+
+# Assignment operaters
+a=25 # here = is a assignement operator
+b=5
+b+=5 # increment the value of a by 5 and then assign it to b
+     # print(b)here 5 will be added to the value of b and output will be 10 
+a-=2 # decrement the value of a by 2 and then assign it to b
+
+# Comparision operators
+
+# (here the output will always return boolean values i.e either true or false)
+a==5 # here == is a comparision operator
+a!=5 # here != is a comparision operator
+a>5 # here > is a comparision operator
+a<5 # here < is a comparision operator
+a>=5 # here >= is a comparision operator
+a<=5 # here <= is a comparision operator
+
+# Logical operators
+# There are some type of l0gical operators like  and, or ,not
+a= True or False
+b= True and True
+c= not(True)
+print (a)
+print (b)
+print (c)
