861b0cc85f56a8472e7fc2ed09a7ea2262268d37
Vaibhav-padharia <160303912+Vaibhav-padharia@users.noreply.github.com>
2025-01-24 22:04:42 +0530

    use of input () and typecasting and type()

---
+++ 06_Type()&Typecasting.py
@@ -0,0 +1,23 @@
+#  type function is used to find he datatype of the variable that we are going to use
+# let's say
+a=35
+t=type (a)
+print(t)
+# the output wll be shown a<class int>
+
+
+# now lets say
+b="jay padhraia"
+t=type(b)
+print(t)
+# here we will get the output as <class str>
+
+# It differs with what type of variable we take as out input
+
+#  we can also use typecasting to convert the datatype of the variable
+# let's say
+a="35"  #here we will get output as string as beacuse we have used double quotes for 
+        # the value of a
+print(type(a))
+
+
---
+++ 07_Input function.py
@@ -0,0 +1,19 @@
+'''
+The input functons allows the user to take input from the keyboard as a string
+
+'''
+a = input("Enter number 1")
+b = input("Enter number 2")
+
+print("Number a is: ", a)
+print("Number b is: ", b)
+
+'''
+The next example is use to enter full name 
+''' 
+
+a=input("first name")
+b=input("second name ")
+
+print("full name is :",a,b)
+
