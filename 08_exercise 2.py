4be9637b2e23b6579b0159781c6813d5116bbae1
Vaibhav-padharia <160303912+Vaibhav-padharia@users.noreply.github.com>
2025-01-27 22:38:45 +0530

    exercise done

--- 08_exercise 2.py
+++ 08_exercise 2.py
@@ -1,1 +1,42 @@
-# write a python program to add two numbers
+#1 write a python program to add two numbers
+
+
+a=int (input("Enter a number:"))
+b=int(input("Enter second number:"))
+
+print("The sum of two numbers is:",a+b)
+
+
+#2 write a python program to find remainder when a number is  divided by 2
+
+a=int (input("Enter a number:"))
+b=int(input("Enter second number:"))
+
+print("the remainder of the proposed number is :" ,a%b)
+
+
+#3 check the type of variable using input function
+
+a=input("Enter first word :")
+b=input("Enter second word:")
+c=(a+b)
+print("The type of the variableis :" ,c,type(c))
+
+
+#4 write a python program to find an average of two numbers entered by user
+
+a=int (input("Enter first number:"))
+b=int (input("Enter second number:"))
+
+c=((a+b)/2)
+
+print("The average of both the numbers is: ",c)
+
+
+#5 write a python program to calculate the square of nuumber entereed by a user 
+a=int (input("Enter first number:"))
+
+c=(a*a)
+
+print("The average of both the numbers is: ",c)
+
