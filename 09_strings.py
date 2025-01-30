9d24f14ea69124b2c41e024a28e2d574ddf77976
Vaibhav-padharia <160303912+Vaibhav-padharia@users.noreply.github.com>
2025-01-30 23:29:46 +0530

    strings

---
+++ 09_strings.py
@@ -0,0 +1,30 @@
+# strings are basically nothing but a sequence of characters
+# theey are enclosed in quotes"" or '' or """ """ or ''' '''
+
+
+# string spacing 
+print("this a concept that is used for geting a part of the string")
+
+# strings are also immutable i.e we cannot change an existing string
+
+# the index of a string starts with (0 to lenghth-1) 
+# hs syntax of string slicing is
+
+
+'''
+sl=name [ind_start:ind_end]    (ind=index)
+         1           n
+
+'''         
+# for example
+
+name="jay kumar padharia"
+    # 0123456789
+
+middlename= name[4:9]
+
+print(middlename) 
+#  here only kumar will be printed beacuse it starts from index 4 and ends at 9
+character=name[4]
+print(character)# will print only character at index 4
+  
