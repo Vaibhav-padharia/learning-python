db3b4736dd43c09185d2aa871ab65fa058aa6954
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-02-11 01:36:22 +0530

    isalpha and isdigit function

---
+++ 10_stringfunctions.py/isalphai+isdigit.py
@@ -0,0 +1,15 @@
+a="jaypadharia"
+print(a.isalpha())
+# In the above we get the o/p as tue beacuse all the elements of the string are alphabets
+
+#now
+
+b="jay 19 padharia"
+print(b.isalpha())
+#Here we get the o/p as false because there is a whitespace and 19 in the string
+
+
+
+c="12345678"
+print(c.isdigit())
+# similarily isdgit() checks if there are any digits 
