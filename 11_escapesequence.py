ff9097282412965421b61fe2471a3a8bc5fe5e01
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-02-11 01:35:44 +0530

    escape sequence

---
+++ 11_escapesequence.py
@@ -0,0 +1,12 @@
+a="Ram is a god\nand his brother name is laxman"
+print(a)
+# here "\n " print the string after it in the next line
+
+# there are  other escape sequence characters aas well they are \
+"""
+\n , \t,  \"\",  \'\'  
+"""
+
+b="Ram is a god\tand his brother name is laxman"
+print(b)
+# this one create a space betwwen them
