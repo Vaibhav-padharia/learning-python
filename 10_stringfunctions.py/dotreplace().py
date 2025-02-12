5c8ad9c7ba9d6b5d777b942d0070c565be78227c
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-02-13 01:05:45 +0530

    .replace function

---
+++ 10_stringfunctions.py/dotreplace().py
@@ -0,0 +1,14 @@
+letter ='''Dear <name>
+ selected
+ <date> '''
+
+print(letter.replace("<name>","Jay").replace("<date>","19 february 2025").replace("selected","we are very happy to select you as our new member on date"))
+
+
+
+'''here we can see that we replaced the comment name with our desired
+string using the (.replace) string function
+All we have to do is write what we want to replce inside " " with what
+
+
+In the above example we can see that we created a chain of replace function '''
