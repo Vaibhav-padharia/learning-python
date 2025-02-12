e60fc1431df438799553d7843b716c57206a45d8
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-02-13 01:04:39 +0530

    fstring function

---
+++ 09_strings/04_f string.py
@@ -0,0 +1,13 @@
+name= input("my name is: ")
+
+print(f"good afternoon {name} ")
+
+
+'''
+this is what we call a fstring where we can concatinate 
+two strings easily but fisrt we have to declare the string as 'fstring'
+we can do that by simply typing a f before what we want to print
+and yes we have to call the required variable inside {}
+'''
+a=input("My name is: ")
+print(f"you are a hero in our eyes {a} ")
