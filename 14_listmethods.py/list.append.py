9a8f83362cf1eab966fcc73c803d0d0153ed291b
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-02-15 01:41:38 +0530

    list mehod append

---
+++ 14_listmethods.py/list.append.py
@@ -0,0 +1,11 @@
+# This method basically add the desired value at the end of the list 
+
+# lets say
+
+friends=["jay","anshuman","swapnil"]
+
+friends.append("tilu")
+print(friends)
+#here we can see that the word tilu is appended at the end of the list
+
+# lists are mutable
