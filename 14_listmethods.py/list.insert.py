76e93367158ecb7a7ccb7147470c3667094baf12
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-02-15 01:42:58 +0530

    list method insert

---
+++ 14_listmethods.py/list.insert.py
@@ -0,0 +1,8 @@
+friends=["jay","badal","soumya"]
+friends.insert(3,"tilu")
+print(friends)
+
+# this method is used to insert the desired value at desired index
+l1=[1,2,23,32]
+l1.insert(4,56)
+print(l1)
