60b6ae6cb7e049db3599dfc0c3b84eaf94226081
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-02-23 21:12:36 +0530

    return

--- 15_tuple.py
+++ 15_tuple.py
@@ -9,23 +9,3 @@ print(type(a))# empty tuple ends with a comma
 b=(1,2,3,"jay","vaibhav")
 print(type(b))   
 
-# tuple are imutable datatype in python
-a=(1,2,3,4,5)
-print(type(a))
-
-# this is what we call a tuple
-a=(1,)
-print(type(a))# empty tuple ends with a comma
-
-b=(1,2,3,"jay","vaibhav")
-print(type(b))   
-# tuple are imutable datatype in python
-a=(1,2,3,4,5)
-print(type(a))
-
-# this is what we call a tuple
-a=(1,)
-print(type(a))# empty tuple ends with a comma
-
-b=(1,2,3,"jay","vaibhav")
-print(type(b))   
