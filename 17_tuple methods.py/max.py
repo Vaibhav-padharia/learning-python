d30aefe2869fb310e0ebe70a097a199fd0ed8377
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-03-08 23:33:35 +0530

    max

---
+++ 17_tuple methods.py/max.py
@@ -0,0 +1,8 @@
+a=(12,1233,43454,456456)
+print(max(a))
+"""
+this function is used to find the max number from the tuple """
+print(type(a))
+
+print(min(a))
+# this is used to find the min of the tuple
