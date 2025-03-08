1282c08d3046987df50d340d23687c64634ead42
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-03-08 23:32:34 +0530

    slicing

---
+++ 18_tupleoperations.py/slicing.py
@@ -0,0 +1,18 @@
+a=(1,34,6567,765,12,3242)
+b=a[2:4]
+print(b)
+
+a=(1,34,6567,765,12,3242)
+b=a[:4]
+print(b)
+# prints the values before the index 4
+
+a=(1,34,6567,765,12,3242)
+b=a[::2]
+print(b)
+# every second element including the first
+
+a=(1,34,6567,765,12,3242)
+b=a[::-1]
+print(b)
+# values printed in revere
