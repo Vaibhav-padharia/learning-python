9ea3c88ca22dd7309f24b8339411c43466a45507
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-03-17 19:57:13 +0530

    practice

--- rough.py
+++ rough.py
@@ -4,3 +4,19 @@ l2=[12,132,454]
 l1.extend(l2)
 # print(this basically joins the value of l2 in l1 
 print(l1) 
+
+
+int=input("Enter your name:")
+a=input("enter your last name:")
+c=(print(int,a))
+print(f"you are a hero in our eyes {c}")
+
+
+
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = f"{first_name} {last_name}"
+print(f"Welcome, {full_name}!")
+print(f"You are a hero in our eyes, {full_name}!")
