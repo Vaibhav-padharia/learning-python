014f5e57d574ded3140764741fe8aceed16b075d
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-03-20 01:16:37 +0530

    new topic-dictionary

---
+++ 20_Dictionary.py
@@ -0,0 +1,17 @@
+"""
+Dictionary is collection of key value pairs
+- these are written in curly braces with values separated by colon(:) just like below
+- properties of dictionary are-: mutable,unorederd,indexe.cannot duplicate values
+
+
+
+"""
+
+name={
+    "vaibhav":100,
+    "jay":12
+}
+print(name,type(name))
+print(name["jay"]) #here we can correspond the print the value of the corresponding pair 
+
+
