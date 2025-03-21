300e215c681ae570381fa622b4649d7d5c316996
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-03-21 22:22:01 +0530

    pop

---
+++ 21_dictmethods.py/dict.pop.py
@@ -0,0 +1,6 @@
+person = {'name': 'Alice', 'age': 25, 'city': 'New York'}
+
+# Removing 'age' and getting its value
+removed_value = person.pop('age')
+print(removed_value)  # Output: 25
+print(person)  # Output: {'name': 'Alice', 'city': 'New York'}
