112a8bad1b5665a6ead57b6906310c1060ac7b7b
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-03-21 22:20:48 +0530

    set

---
+++ 22_Sets.py
@@ -0,0 +1,7 @@
+# A set is a collection of well defined objects
+# we can declare it to be a set using the set()
+# because both the dictionary and set have the same syntax
+
+a={"jay","hidy","hrht"}
+a=set()
+print(type(a))
