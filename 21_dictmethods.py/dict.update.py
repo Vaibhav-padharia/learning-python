2e5ae4dc1d2b99c51f5c73ad1360aa801f5d8dc2
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-03-21 22:21:46 +0530

    dcit update

---
+++ 21_dictmethods.py/dict.update.py
@@ -0,0 +1,11 @@
+
+name={
+    "vaibhav":100,
+    "jay":12
+}
+
+
+print(name.update({"vaibhav":89, "vipul":45}))
+print(name)
+# this basically updates  all the desired  values from the dictionary
+# this even add if we wnat to add any item in the dictionary here as we added vipul with value 45
