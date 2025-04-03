aff44af2107914d24116c3263ea724319f6986f2
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-04-03 23:46:17 +0530

    practice

--- rough.py
+++ rough.py
@@ -20,4 +20,17 @@ last_name = input("Enter your last name: ")
 full_name = f"{first_name} {last_name}"
 print(f"Welcome, {full_name}!")
 print(f"You are a hero in our eyes, {full_name}!")
-bhfwqrfsjdfrfi 
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = f"{first_name} {last_name}"
+print(f"Welcome, {full_name}!")
+print(f"You are a hero in our eyes, {full_name}!")
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = f"{first_name} {last_name}"
+print(f"Welcome, {full_name}!")
+print(f"You are a hero in our eyes, {full_name}!")
+
+
